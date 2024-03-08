b80e2936ce9d386a7e50b945687f64aec54b6b42
Deepika <82407112+Deepika0701@users.noreply.github.com>
2024-03-08 23:20:09 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Button } from './components/ui/button'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import Roles from './pages/Roles'
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Roles />} />
+        <Route path="/" element={<Login/>} />
+        <Route path="/Dashboard" element={<Dashboard/>} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
