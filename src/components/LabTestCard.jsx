f0bb355f1473d635a00364c43cec32da64513e3e
Deepika <82407112+Deepika0701@users.noreply.github.com>
2024-03-16 22:45:14 +0530

    login and test pageee

--- src/components/LabTestCard.jsx
+++ src/components/LabTestCard.jsx
@@ -4,7 +4,7 @@ const LabTestCard = ({test ,handleCart}) => {
   const {testName,price,description,img}=test;
   return (
     <div className="max-w-sm max-h-72 rounded overflow-hidden shadow-lg">
-     
+     {/* hye  */}
       <div className="px-6 py-4 flex flex-row gap-4">
       <img className="w-20 h-20" src={img} alt={testName} />
       <div>
